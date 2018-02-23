edfbf691e600beaf820331f3761b0c4747615e3d
mashranga <mashranga@yahoo.com>
2018-02-23 13:24:36 +0100

    Fix problem on ISS_compare

--- R/13.1.1_ISS_compare.R
+++ R/13.1.1_ISS_compare.R
@@ -2,18 +2,20 @@
 #' Compare ISS data
 #' 
 #' @description Compare multiple ISS data
-#'
+#' @param ... Input data in class RCA_class. At least 2 dataset or more. Output of \link[MolDia]{readRCA}.
+#' @param logdata log2 apply to data or not. Deafult is FALSE.
+#' @param label Label each point. Default is TRUE.
+#' @param levelCI Level of confidence interval to use. Default is 0.95
+#' @param live Show plot in interactive mode. Default is FALSE
 #' 
 #' @examples 
 #' hc_left  <- readRCA(file = system.file("extdata", "Hypocampus_left.csv", package="MolDia"),  cellid = "CellId",centX = "centroid_x", centY = "centroid_y")
 #' hc_right <- readRCA(file = system.file("extdata", "Hypocampus_right.csv", package="MolDia"), cellid = "CellId",centX = "centroid_x", centY = "centroid_y")
-#' 
-#' kk<- ISS_compare(hc_left,hc_right,hc_left,hc_right, label = F, levelCI = 0.99, live = F, logdata = FALSE)
+#' kk<- ISS_compare(hc_left,hc_right, label = F, levelCI = 0.99, live = F, logdata = FALSE)
 #' 
 #' @export
-ISS_compare <- function(..., logdata = FALSE, label = TRUE, levelCI = 0.95, live = TRUE)
+ISS_compare <- function(..., logdata = FALSE, label = TRUE, levelCI = 0.95, live = FALSE)
 {
-  options(warn=2)
   ## Main data
   maindata <- list(...)
   ## Stopping criteria
@@ -31,26 +33,19 @@ ISS_compare <- function(..., logdata = FALSE, label = TRUE, levelCI = 0.95, live
   names(maindata)<- paste(paste0("data",1:length(maindata)))
   
   ## Merge dataset by probe name
-  merge.all <- function(x, y) {
-    merge(x, y, all=TRUE, by="probe")
-  }
-  maindata <- Reduce(merge.all, maindata)
+  merge.all <- function(x, y) {merge(x, y, all=TRUE, by="probe") }
+  maindata  <- Reduce(merge.all, maindata)
   probe <- maindata$probe
   maindata$probe <- NULL
   
   ## Take all possible combinition
   mypairs <- t(combn(length(maindata),2))
   mypairs <- lapply(apply(mypairs,1,list),unlist)
-  mypairs_name <- lapply(mypairs, function(i)
-  {
-    paste(paste0("Data.",i), collapse = "_")
-    
-  })
+  mypairs_name <- lapply(mypairs, function(i) {paste(paste0("Data.",i), collapse = "_")})
   mypairs <- lapply(mypairs, function(i) 
     {
     pp<- maindata[i]
     pp$probe <- probe
-    #colnames(pp) <- c("d1","d2","probe")
     pp
     })
   names(mypairs) <- mypairs_name
@@ -76,22 +71,22 @@ ISS_compare <- function(..., logdata = FALSE, label = TRUE, levelCI = 0.95, live
     m <- suppressMessages(lm(f, data= mypairs[[i]]))
 
         ## Plot
-    pp<- ggplot(data = mypairs[[i]], aes_string(x = d1, y = d2,label = probe)) +
-      {if(label) geom_text()}  +
-      geom_point(color='blue') +
-      suppressMessages(geom_smooth(method = "lm", se = T, show.legend = TRUE, level = levelCI)) + 
-      suppressWarnings(labs(title = names(mypairs[i]), subtitle = substitute(italic(y) == a + b %.% italic(x)*","~~italic(R)^2~"="~r2, 
+    pp<- ggplot2::ggplot(data = mypairs[[i]], ggplot2::aes_string(x = d1, y = d2,label = probe)) +
+      {if(label) ggplot2::geom_text()}  +
+      ggplot2::geom_point(color='blue') +
+      suppressMessages(ggplot2::geom_smooth(method = "lm", se = T, show.legend = TRUE, level = levelCI)) + 
+      suppressWarnings(ggplot2::labs(title = names(mypairs[i]), subtitle = substitute(italic(y) == a + b %.% italic(x)*","~~italic(R)^2~"="~r2, 
                                                             list(a = format(coef(m)[1], digits = 2), 
                                                                  b = format(coef(m)[2], digits = 2), 
                                                                  r2 = format(summary(m)$r.squared, digits = 3))))) +
-      scale_y_log10() +
-      scale_x_log10()
+      ggplot2::scale_y_log10() +
+      ggplot2::scale_x_log10()
     pp
     
   })
   
   ## Return multiple plot or single plot 
   if(live & length(mypairs)==1) print(ggplotly(myggplot[[1]]))
-  else grid.arrange(grobs = myggplot)
-  return(mypairs)
+  else gridExtra::grid.arrange(grobs = myggplot)
+  return(NULL)
 }
