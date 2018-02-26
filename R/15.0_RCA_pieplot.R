282c1c2dad21e2b8c6498086d2e118bdf821ec65
mashranga <mashranga@yahoo.com>
2018-02-26 18:59:05 +0100

    pie plot

--- R/15.0_RCA_pieplot.R
+++ R/15.0_RCA_pieplot.R
@@ -1,10 +1,8 @@
-"RCA_pieplot"
+"ISS_pieplot"
 #' Venn-pie chart on RCA data based on genes of interest
 #'
 #' @param data Input data in class RCA_class. Output of \link[MolDia]{readRCA}.
 #' @param gene Gene of interest. Default is NULL.
-#' @param gene_with Consider cells only with these genes. Default is NULL.
-#' @param gene_without Consider cells without these genes. Default is NULL.
 #'
 #' @return The Venn-pie plot.
 #'
@@ -14,7 +12,7 @@
 #' @examples
 #'########## Reading data
 #' data_1      <- readRCA(file = system.file("extdata", "CellBlobs_QT_0.35.csv", package="MolDia"),
-#'                        cellid = "CellID")
+#'                        cellid = "CellID",centX = "centroidX", centY = "centroidY")
 #'
 #' ## Define marker gene group
 #' marker_gene <- data_1@gene
@@ -42,17 +40,97 @@
 #'
 #' #####  Pie chart of all Circular mRNA in cell with at least 3 neuronal genes.
 #' genes <- marker_gene1[[1]]
-#' res   <- RCA_pieplot (data = mydata, gene = genes)
-#'
-#' #####  All circular mRNA with at least 3 neuronal genes with either "Plp1.L" and "Map2.L"
-#' withgene <- c("Plp1.L","Map2.L")
-#' res   <- RCA_pieplot (data = mydata, gene = genes, gene_with = withgene)
-#'
-#' #####  All circular mRNA with at least 3 neuronal genes without both "Plp1.L" and "Map2.L"
-#' withoutgene <- c("Plp1.L","Map2.L")
-#' res   <- RCA_pieplot (data = mydata, gene = genes, gene_without = withoutgene)
+#' res   <- ISS_pieplot (data = mydata, gene = genes)
 #'
 #' @export
+#' 
+ISS_pieplot <- function(data, gene= NULL)
+{
+  ## Main data 
+  main_data <- data
+  data <- data@data
+  
+  ## Select genes
+  if (length(gene)==0 ) data <- data
+  else {
+    data <- data[,gene, drop= FALSE]}
+  
+  ## Check gene name in data
+  if(all(gene %in%colnames(data))==FALSE) stop("Check gene name are same and equel length in both input ", call. = FALSE)
+  
+  ## Delete cell with no gene
+  data <- data[rowSums(data) > 0,]
+  data <- data[,colSums(data) > 0]
+  
+  ## Gene name
+  gname <-colnames(data)
+  
+  ## Get combinition of gene name (Gene count > 0) for each cell and count frequency of gene combinition
+  data <- apply(data,1,function(i)
+  {
+    pp<- paste0(colnames(data)[i>0],collapse = "-")
+  })
+  data <- data.frame(table(unlist(data)))
+  colnames(data) <- c("Gene","Count")
+  data <- data[order(data$Count, decreasing = T),]
+  
+  ## Sort gene name in each gene name according their high to low occurance in gene combinition
+  tot_comb  <- as.vector(data$Gene)
+  tot_comb1 <- strsplit(tot_comb,"-")
+  tot_comb2 <- unique(unlist(tot_comb1))
+  
+  g  <- rep(seq_along(tot_comb1), sapply(tot_comb1, length))
+  m  <- lapply(tot_comb2, function(x) g[which(unlist(tot_comb1) %in% x)])
+  names(m) <- tot_comb2
+  m1 <- sort(unlist(lapply(m,function(i) sum(data$Count[i]))), decreasing = TRUE)
+  m2 <- names(m1)
+  
+  ll <- lapply (tot_comb1, function(i) { m2[sort(match(i,m2))] })
+  ll1 <- unlist (lapply(ll, paste0, collapse= "-"))
+  
+  data$Gene <- ll1
+  
+  ## Apply Venn-pie plot
+  if(length(gene) == 0 )
+  {
+    set.seed(1)  ## This random seed will keep the constant color reproduce
+    res <- sunburstR::sunburst(data, count = T, legendOrder = sort(gname))
+  } else {
+    set.seed(1)
+    cols <- as.list(randomcoloR::distinctColorPalette(length(gene)))
+    names(cols) <- NULL
+    
+  ## Custom Messege
+    custom.message = "function (d) {
+    root = d;
+    while (root.parent) {
+    root = root.parent
+    }
+    p = (100*d.value/root.value).toPrecision(3);
+    msg = p+' %<br/>'+d.value+' of '+root.value;
+    return msg;
+  }"
+    
+    
+    
+    set.seed(1)  ## This random seed will keep the constant color reproduce
+    res <- sunburstR::sunburst(data, count = T, percent = T,explanation = custom.message,
+                               legendOrder = sort(gname),
+                               colors = list(range  = cols,
+                                             domain = gene))
+}
+  print(res)
+  
+  ## return RCA object
+  myres <- methods::new("RCA_class",
+                        data     = data,
+                        location = main_data@location[rownames(data),],
+                        gene = colnames(data))
+  return(data)
+  
+}
+
+################################################################################
 RCA_pieplot <- function(data, gene = NULL, gene_with = NULL, gene_without = NULL)
   {
   ## Data select
